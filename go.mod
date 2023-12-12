57815ead7095c4a873289fe84f85a87d01ed7139
Gavin Wang <79772347+wry0313@users.noreply.github.com>
2023-12-11 19:05:45 -0800

    change to unix

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module syscall-golang
 
 go 1.21.1
+
+require golang.org/x/sys v0.15.0
--- main.go
+++ main.go
@@ -1,27 +1,28 @@
 package main
 
 import (
-	"syscall"
+	"golang.org/x/sys/unix"
 )
 
 func main() {
 	// execute a syscall for opening a file descriptor
-	fd, err := syscall.Open("./hello", syscall.O_RDONLY, 0)
+	fd, err := unix.Open("./hello", unix.O_RDONLY, 0)
 	if err != nil {
 		panic(err)
 	}
 	// read the file descriptor
     buf := make([]byte, 1024) // Adjust the size as needed
-	_, err = syscall.Read(fd, buf)
+	_, err = unix.Read(fd, buf)
 	if err != nil {
 		panic(err)
 	}
 	// print the content
 	println(string(buf))
 	// execute a syscall for closing a file descriptor
-	err = syscall.Close(fd)
+	err = unix.Close(fd)
 	if err != nil {
 		panic(err)
 	}
+
 
 }
